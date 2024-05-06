419f79dd47b2def994c3b17759441b118514c3f2
Juliya Lipatova <575@ukr.net>
2024-05-06 16:11:23 +0200

    script added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
-import './app.js'
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
 const app = express();
+app.use(cors());
+app.use(express.json())
 
+const { BD_Host } = process.env;
 
+const PORT =  BD_Host || 8800;
 
-
-app.listen(8800, () => {
-  console.log("Connected");
-});
+mongoose
+  .connect(BD_Host)
+  .then(() => {
+    console.log("connected to base");
+    app.listen(PORT, () => {
+      console.log("Connected");
+    });
+  })
+  .catch((error) => {
+    console.log("error", error.message);
+    process.exit(1);
+  });
